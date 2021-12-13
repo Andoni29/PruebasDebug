48e3be8be4c4b02d3763e87b232d8e857b8cbdaa
nada <nada>
2021-12-13 13:11:35 +0100

    factorial debugeada

--- src/PruebaDebug.java
+++ src/PruebaDebug.java
@@ -1,9 +1,22 @@
 
 public class PruebaDebug {
 
+	public static int calcFactorial(int n) {
+		
+		for(int i=n-1;i>0;i--) {
+			n=n*i;
+		}
+		return n;
+	}
 	public static void main(String[] args) {
 		
+		int num;
 		
+		System.out.print("Introduce un numero: ");
+		num=Console.readInt();
+		
+		num=calcFactorial(num);
+		System.out.println(num);
 
 	}
 }
